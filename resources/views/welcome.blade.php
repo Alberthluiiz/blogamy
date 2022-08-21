fb5d3922a136a01254330bca59e6856d8b8ca8b9
Luis Guillen <alberto_29luis@hotmail.com>
2022-08-20 21:33:07 -0500

    Modificacion del Archivo Welcomw

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Este nos permite darle un nombre a nuestra Paguina web -->
-        <title>Amy</title>
+        <title>Tienda</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
